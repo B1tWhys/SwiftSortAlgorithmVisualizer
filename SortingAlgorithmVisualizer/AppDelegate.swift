2c9438b6eb22d7592aeaff5eae45fdf6f4bfc15e
Skyler Arnold <skyarnold1@me.com>
2017-02-06 14:21:38 -0700

    Initial Commit

---
+++ SortingAlgorithmVisualizer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SortingAlgorithmVisualizer
+//
+//  Created by Skyler Arnold on 2/6/17.
+//  Copyright © 2017 Skyler Arnold. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
