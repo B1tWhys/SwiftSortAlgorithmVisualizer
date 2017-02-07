0e224ab2e023f77cb3dbf95bcb3f9462e4466700
Skyler Arnold <skyarnold1@me.com>
2017-02-07 01:18:17 -0700

    Added view class

---
+++ GraphView.swift
@@ -0,0 +1,15 @@
+//
+//  GraphView.swift
+//  SortingAlgorithmVisualizer
+//
+//  Created by Skyler Arnold on 2/6/17.
+//  Copyright © 2017 Skyler Arnold. All rights reserved.
+//
+
+import Cocoa
+
+class GraphView: NSView {
+	override func draw(_ dirtyRect: NSRect) {
+		
+	}
+}
