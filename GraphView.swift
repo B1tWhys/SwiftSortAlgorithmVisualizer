ac819090124085281f220d13e352a4f57a4de45b
bouldersky <skyarnold1@me.com>
2017-02-08 03:49:52 -0700

    Added Swap objects, extended NSArray to add the swap method, and I implemented the graphView. To display data, you just modify the graphView.values array. It will handle everything :)

--- GraphView.swift
+++ GraphView.swift
@@ -9,7 +9,60 @@
 import Cocoa
 
 class GraphView: NSView {
-	override func draw(_ dirtyRect: NSRect) {
+	
+	var values: [Int]! {
+		didSet {
+			if (values.count != 0) {
+				Swift.print("valuesSet")
+				self.setNeedsDisplay(self.frame)
+			}
+		}
+	}
+	
+	var barWidth: Float {
+		get {
+			return Float(self.frame.width)/Float(values.count)
+		}
+	}
+	var hScale: Float {
+		get {
+			let range = Float(values.max()!)
+			return Float(self.frame.height)/range
+		}
+	}
+	
+	
+	override init(frame frameRect: NSRect) {
+		self.values = [Int]()
+
+		super.init(frame: frameRect)
+	}
+	
+	convenience init(frame frameRect: NSRect, values: [Int]) {
+		self.init(frame: frameRect)
 		
+		self.values = values
+
+	}
+	
+	required init?(coder: NSCoder) {
+		super.init(coder: coder)
+	}
+	
+	override func draw(_ dirtyRect: NSRect) {
+		NSColor.black.setFill()
+		NSBezierPath.fill(dirtyRect)
+		NSColor.white.setFill()
+		for index in 0..<self.values.count {
+			let val = self.values[index]
+			let height = CGFloat(Float(val)*self.hScale)
+			let x = CGFloat(Float(index)*barWidth)
+			
+			let rect = CGRect(x: x,
+			                  y: 0.0,
+			                  width: CGFloat(self.barWidth),
+			                  height: height)
+			NSBezierPath.fill(rect)
+		}
 	}
 }
--- SortingAlgorithmVisualizer/ViewController.swift
+++ SortingAlgorithmVisualizer/ViewController.swift
@@ -9,13 +9,25 @@
 import Cocoa
 
 class ViewController: NSViewController {
+	@IBOutlet weak var graphView: GraphView!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-
+		self.graphView.values = [1, 2, 3, 4, 5]
+		self.view.window?.backgroundColor = NSColor.black
+		
+		Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(addValToChart), userInfo: nil, repeats: true)
+		
 		// Do any additional setup after loading the view.
 	}
 
+	func addValToChart() {
+		let swpObj = Swap(index1: 2, index2: 3)
+		
+		self.graphView.values.swap(swapObj: swpObj)
+		//self.graphView.values.append(Int(val))
+	}
+	
 	override var representedObject: Any? {
 		didSet {
 		// Update the view, if already loaded.
